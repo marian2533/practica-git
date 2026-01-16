607b13ae6d54ca8eb11ae9f44ff93e79ab07f5a8
Dynamoxex <dynamoxex@gmail.com>
2026-01-16 17:12:10 -0300

    se agrego BN.py

---
+++ BN.py
@@ -0,0 +1,35 @@
+All Boys 
+
+Almagro 
+
+Alvarado
+ 
+Arsenal
+
+Atlanta,
+
+Colegiales,
+
+Deportivo Madryn,
+
+Deportivo Maipú
+
+Ferro Carril Oeste
+
+Gimnasia y Tiro
+
+Güemes (Santiago del Estero)
+
+Los Andes
+
+Patronato
+
+Quilmes
+
+Racing (Córdoba)
+
+San Martín (Tucumán)
+
+San Miguel
+
+Tristán Suárez
