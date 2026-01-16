7b15254b2e61eb46b56e7526332d3d97537d85b2
Dynamoxex <dynamoxex@gmail.com>
2026-01-16 17:15:00 -0300

    Se agrego BN1.py

---
+++ BN1.py
@@ -0,0 +1,29 @@
+Agropecuario
+
+Almirante Brown
+
+Central Norte
+
+Chacarita Juniors
+
+Chaco For Ever
+
+Colón (Santa Fe)
+
+Defensores de Belgrano
+
+Defensores Unidos
+
+Gimnasia y Esgrima (Jujuy)
+
+Gimnasia y Esgrima de Mendoza
+
+Mitre (Santiago del Estero)
+
+Nueva Chicago
+
+Talleres (Remedios de Escalada)
+
+Temperley
+
+San Telmo
